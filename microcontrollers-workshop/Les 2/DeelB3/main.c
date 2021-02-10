61ca389323a0a0fcf569611bb5a629b1ec25ed61
NLR <nkw.leroux@student.avans.nl>
2021-02-10 12:40:09 +0100

    WIP

--- microcontrollers-workshop/Les 2/DeelB3/main.c
+++ microcontrollers-workshop/Les 2/DeelB3/main.c
@@ -10,7 +10,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-const unsigned char Numbers [10] = {
+const unsigned char Numbers [10][2] = {
 	// dPgfe dcba
 	0b00111111, // 0
 	0b00000110, // 1
@@ -22,6 +22,13 @@ const unsigned char Numbers [10] = {
 	0b00000111, // 7
 	0b01111111, // 8
 	0b01101111, // 9
+	0b01110111,	// 10 (A)
+	0b01111100,	// 11 (B)
+	0b00111001,	// 12 (C)
+	0b01011110,	// 13 (D)
+	0b01111001,	// 14 (E)
+	0b01110001,	// 15 (F)
+			// 16 (G) Must not be displayed since its bigger than 15.
 };
 
 void display(int digit);
@@ -34,19 +41,20 @@ int main(void)
     /* Replace with your application code */
     while (1) 
     {
-		for (int i = 0; i < 10; i++)
+		for (int i = 0; i < 17; i++)
 		{
-			PORTC= Numbers[i];
-			wait(100);
+			display(i);
+			
 		}
     }
 }
 
 void display(int digit){
 	if(0 <= digit || digit <= 15){
-		
+		PORTC= Numbers[digit];
+		wait(100);
 	}else{
-		
+		PORTC= 0b01111011;
 	}
 	
 }
