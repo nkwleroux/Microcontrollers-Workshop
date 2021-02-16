01de617c6e1704e8cdd4f62acc513b1cb6f914ad
NLR <nkw.leroux@student.avans.nl>
2021-02-16 12:27:44 +0100

    Start opdracht 5

---
+++ microcontrollers-workshop/Les 2/DeelB5/main.c
@@ -0,0 +1,18 @@
+/*
+ * DeelB5.c
+ *
+ * Created: 16/2/2021 12:27:04 PM
+ * Author : Nic
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
