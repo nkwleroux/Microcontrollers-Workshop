e5609bda28321d70a3a744acac7f8304bc20da2c
NLR <nkw.leroux@student.avans.nl>
2021-02-24 11:33:48 +0100

    Stage opdracht 3

---
+++ microcontrollers-workshop/Les 3/DeelB3/main.c
@@ -0,0 +1,18 @@
+/*
+ * DeelB3.c
+ *
+ * Created: 24/2/2021 11:33:15 AM
+ * Author : Nic
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
