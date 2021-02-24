78c8dc50de1d22c8e0fb817575afd1c3ed9c4ccd
NLR <nkw.leroux@student.avans.nl>
2021-02-24 08:58:15 +0100

    Stage les 3 solution

---
+++ microcontrollers-workshop/Les 3/DeelB1/main.c
@@ -0,0 +1,18 @@
+/*
+ * DeelB1.c
+ *
+ * Created: 24/2/2021 8:56:56 AM
+ * Author : Nic
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
