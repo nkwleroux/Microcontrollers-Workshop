096a12c99bce340ff6262c2505774816ad2ef60a
NLR <nkw.leroux@student.avans.nl>
2021-03-10 11:42:27 +0100

    init project

---
+++ microcontrollers-workshop/Les 4/DeelB2/main.c
@@ -0,0 +1,18 @@
+/*
+ * DeelB2.c
+ *
+ * Created: 10/3/2021 11:42:15 AM
+ * Author : Nic
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
