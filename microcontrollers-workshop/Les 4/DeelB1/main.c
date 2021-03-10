8bcbbab61720c7155609c320891f54eb6c3d9c5e
NLR <nkw.leroux@student.avans.nl>
2021-03-10 11:14:52 +0100

    Init solution

---
+++ microcontrollers-workshop/Les 4/DeelB1/main.c
@@ -0,0 +1,18 @@
+/*
+ * DeelB1.c
+ *
+ * Created: 10/3/2021 11:14:13 AM
+ * Author : Nic
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
