c0df5b0bae7c19402a0b75af26ed5dbe480028c9
NLR <nkw.leroux@student.avans.nl>
2021-03-13 12:26:12 +0100

    WIP

--- microcontrollers-workshop/Les 4/DeelB3/main.c
+++ microcontrollers-workshop/Les 4/DeelB3/main.c
@@ -10,11 +10,11 @@
 #include "LCD/LCD.h"
 
 void init_adc(void){
-	ADMUX = 0b11100001;		// (7,6)	11 = REFS1:0 (??)
+	ADMUX = 0b01100011;		// (7,6)	11 = REFS1:0 (??)
 							// (5)		1 = ADLAR - left adjust result
 							// (4-0)	00001 = MUX4:0 (ADC 1) 
 	
-	ADCSRA = 0b10000110;	// (7)		1 = ADEN (Enables ADC)
+	ADCSRA = 0b11000110;	// (7)		1 = ADEN (Enables ADC)
 							// (6)		0 = ADSC (Single conversion mode false)
 							// (5)		0 = ADFR (ADC in free running mode false)
 							// (4)		0 = ADIF (ADIF interrupt disabled)
@@ -27,7 +27,7 @@ int main(void)
 	init_lcd();
 	wait(10);
 	DDRF = 0x00; //input
-	DDRD = 0xff; //output
+	DDRD = 0xFF; //output
 	init_adc();
 	wait(10);
 	
@@ -40,8 +40,8 @@ int main(void)
 		ADCSRA ^= 1 << 6;				
 		wait(100);
 		PORTD = ADCH; //8 bits.
-		char str[10];
-		sprintf(str, "%d", ADCH);
+		char str[12];
+		sprintf(str, "%d", ADCH<<1);
 		display_text(str);
 		wait(500);
 		lcd_clear();
