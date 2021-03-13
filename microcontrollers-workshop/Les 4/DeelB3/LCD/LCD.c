908fd6cc0bc4fbec092e7c0d1e5e4b0d8b461d8b
NLR <nkw.leroux@student.avans.nl>
2021-03-13 11:54:36 +0100

    WIP

---
+++ microcontrollers-workshop/Les 4/DeelB3/LCD/LCD.c
@@ -0,0 +1,95 @@
+/*
+ * LCDc.c
+ *
+ * Created: 13/3/2021 11:28:41 AM
+ *  Author: Nic & Jorn
+ */ 
+
+#define F_CPU 8e6
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include "LCD.h"
+
+void wait( int ms ) {
+	for (int i=0; i<ms; i++) {
+		_delay_ms( 1 );		// library function (max 30 ms at 8MHz)
+	}
+}
+
+void init_lcd( void ) {
+	DDRC = 0xFF;
+	DDRA = 0xFF;
+	PORTC = 0x00;
+	PORTA = 0x00;
+
+	PORTC = 0x20;
+	lcd_strobe_lcd_e();
+
+	PORTC = 0x20;
+	lcd_strobe_lcd_e();
+	
+	PORTC = 0x80;
+	lcd_strobe_lcd_e();
+
+	PORTC = 0x00;
+	lcd_strobe_lcd_e();
+	
+	PORTC = 0xF0;
+	lcd_strobe_lcd_e();
+
+	PORTC = 0x00;
+	lcd_strobe_lcd_e();
+	PORTC = 0x60;
+	lcd_strobe_lcd_e();
+	
+	lcd_clear();
+}
+
+void lcd_strobe_lcd_e(void) {
+	PORTA |= (1<<LCD_E);	// E high
+	_delay_ms(1);
+	PORTA &= ~(1<<LCD_E);  	// E low
+	_delay_ms(1);
+}
+
+void lcd_write_data(unsigned char byte ) {
+	// First nibble.
+	PORTC = byte;
+	PORTA |= (1<<LCD_RS);
+	lcd_strobe_lcd_e();
+
+	// Second nibble
+	PORTC = (byte<<4);
+	PORTA |= (1<<LCD_RS);
+	lcd_strobe_lcd_e();
+}
+
+void lcd_write_command(unsigned char byte) {
+	// First nibble.
+	PORTC = byte;
+	PORTA &= ~(1<<LCD_RS);
+	lcd_strobe_lcd_e();
+
+	// Second nibble
+	PORTC = (byte<<4);
+	PORTA &= ~(1<<LCD_RS);
+	lcd_strobe_lcd_e();
+}
+
+void lcd_clear() {
+	lcd_write_command (0x01); 
+	_delay_ms(2);
+	lcd_write_command (0x80); 
+}
+
+void display_text(char *str) {
+	for(;*str; str++){
+		lcd_write_data(*str);
+	}
+}
+
+void set_cursor( int position ) {
+	lcd_write_command(0x80 | position);
+}
---
+++ microcontrollers-workshop/Les 4/DeelB3/main.c
@@ -0,0 +1,43 @@
+/*
+ * DeelB3.c
+ *
+ * Created: 13/3/2021 11:28:09 AM
+ * Author : Nic & Jorn
+ */ 
+
+#include <avr/io.h>
+#include <stdio.h>
+#include "LCD/LCD.h"
+
+void init_adc(void){
+	ADMUX = 0b11100001;		// (7,6)	11 = REFS1:0 (??)
+							// (5)		1 = ADLAR - left adjust result
+							// (4-0)	00001 = MUX4:0 (ADC 1) 
+	
+	ADCSRA = 0b11100110;	// (7)		1 = ADEN (Enables ADC)
+							// (6)		0 = ADSC (Single conversion mode false)
+							// (5)		0 = ADFR (ADC in free running mode false)
+							// (4)		0 = ADIF (ADIF interrupt disabled)
+							// (3)		0 = ADIE (ADC conversion complete interrupt disabled)
+							// (2-0)	110 = ADPS2:0 (64)
+}
+
+int main(void)
+{
+	init_lcd();
+	DDRF = 0x00;
+	DDRD = 0xff;
+	init_adc();
+	
+    while (1) 
+    {
+		ADCSRA |= 1 << 6;				
+		while ( ADCSRA & 1 << 6) ;		
+		PORTD = ADCH; //8 bits.
+		char str[10];
+		sprintf(str, "%d", ADCH);
+		display_text(str);
+		wait(500);
+    }
+}
+
